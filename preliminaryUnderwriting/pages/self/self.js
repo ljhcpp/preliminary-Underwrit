77f25541e034ac7fb6c959a0c946ad82c03fdf52
lujunhui <lujunhui_c@163.com>
2019-07-22 15:33:11 +0800

    版本回退

---
+++ preliminaryUnderwriting/pages/self/self.js
@@ -0,0 +1,136 @@
+
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    array: null ,
+    index:'' ,
+    employeeId: '',
+    companyArray:null,
+    login_msg:'',
+  },
+
+  onLoad: function () {
+    console.log(app.globalData.userInfo)
+    
+    // 请求公司名称
+    wx.request({
+      url: 'http://underwriting.algerfan.cn/company/select',
+      method: 'get',
+      header: {
+        'content-type': 'application/json' 
+      },
+      success: res => {
+        let companyList=res.data.companyList;
+        let companyArray =[];
+        if(res.data.status==1){
+          for(let i in companyList){
+            companyArray.push(companyList[i].company);
+          }
+          this.setData({
+            array: companyList,
+            companyArray:companyArray
+          })
+        }
+      }
+    })
+    // 进入时判断globaldata.userinfo 是否有用户信息
+    if (app.globalData.userInfo != null) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+      wx.switchTab({
+        url: './../index/index'
+      })
+    }
+  },
+  // bindKeycompany: function (e) {
+  //   let val = e.detail.value;
+  //   this.setData({
+  //     company: val
+  //   })
+  // },
+  bindKeyemployee: function (e) {
+    let val = e.detail.value;
+    this.setData({
+      employeeId: val
+    })
+  },
+  // 工号选择
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', this.data.array[e.detail.value]);
+    this.setData({
+      index: e.detail.value,
+    })
+  },
+  // 登录请求获得openid
+  login: function (e) {
+    let that = this;
+    if (that.data.employeeId != '' && that.data.company != '') {
+      let employeeId = that.data.employeeId;
+      let company = that.data.companyArray[that.data.index];
+      wx.login({
+        success: function (res) {
+          var code = res.code;//登录凭证
+          if (code) {
+            //2、调用获取用户信息接口
+            wx.getUserInfo({
+              success: function (res) {
+                //3.请求自己的服务器，解密用户信息 获取unionId等加密信
+                wx.request({
+                  url: 'http://underwriting.algerfan.cn/agent/login',
+                  data: {
+                    employeeId: employeeId,
+                    company: company,
+                    encryptedData: res.encryptedData,
+                    iv: res.iv,
+                    code: code
+                  },
+                  method: 'post',
+                  header: {
+                    'content-type': 'application/x-www-form-urlencoded',
+                  },
+                  success: function (res) {
+                      console.log(res);
+                      if(res.data.status ==1){
+                        app.globalData.userInfo = res.data.userInfo;
+                        that.setData({
+                          userInfo: res.data.userInfo,
+                          hasUserInfo: true,
+                          login_msg:res.data.msg
+                        });
+                        wx.switchTab({
+                          url: './../index/index'
+                        })
+                      }else{
+                        that.setData({
+                          login_msg:res.data.msg
+                        });
+                      }
+                  }
+                })
+
+              },
+              fail: function () {
+                console.log('获取用户信息失败')
+              }
+            })
+          } else {
+            console.log('获取用户登录态失败！' + r.errMsg)
+          }
+        },
+        fail: function () {
+          that.setData({
+            login_msg: '登录失败'
+          });
+        }
+      })
+    }
+  }
+})
---
+++ preliminaryUnderwriting/pages/submit/submit.js
@@ -0,0 +1,178 @@
+// pages/submit/submit.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      Gender: ['男', '女'],
+      encryptedData: '',
+      iv: '',
+      code: '',
+      phone: ''
+  },
+  /*
+   *性别选择
+   */
+    bindPickerChange(e) {
+        this.setData({
+            index: e.detail.value
+        })
+    },
+    /*
+   *日期选择
+   */
+    bindDateChange(e) {
+        this.setData({
+            date: e.detail.value
+        })
+    },
+    /* 
+     *表单提交数据 
+    */
+    formSubmit: function (e) {
+        var Foo = this;
+        var name = /^[\u4E00-\u9FA5]{2,4}$/;
+        if (e.detail.name == '' || !name.test(e.detail.value.name)){
+            wx.showModal({
+                title: '提示',
+                content: '请输入有效的核保人姓名！',
+            })
+        } else if (e.detail.value.sex != 0 && e.detail.value.sex != 1){
+            wx.showModal({
+                title: '提示',
+                content: '请完善核保人性别！',
+            })
+        }else if(e.detail.value.birthday==''||!e.detail.value.birthday){
+            wx.showModal({
+                title: '提示',
+                content: '请完善核保人出生年月！',
+            })
+        } else if (e.detail.value.introduce == '' || !e.detail.value.introduce){
+            wx.showModal({
+                title: '提示',
+                content: '请对核保人健康状况进行简单描述',
+            })
+        }else{
+            console.log(e.detail.value.phone);
+            wx.request({
+                url: 'http://underwriting.algerfan.cn/underwriting/insert',
+                header: {
+                    'content-type': 'application/x-www-form-urlencoded'
+                },
+                method: 'POST',
+                data: {
+                    'formId': e.detail.formId,
+                    'name': e.detail.value.name,
+                    'sex': e.detail.value.sex == 0 ? 'male' : 'female',
+                    'birthday': e.detail.value.birthday,
+                    'phone': e.detail.value.phone,
+                    'introduce': e.detail.value.introduce,
+                    'encryptedData': this.data.encryptedData,
+                    'iv': this.data.iv,
+                    'code': this.data.code
+                },
+                success: function (res) {
+                    wx.showModal({
+                        title: '提示',
+                        content: res.data.msg,
+                        success: function (res) {
+                            Foo.setData({
+                                form_info: '',
+                                index:'',
+                                date:'',
+                                phone:''
+                            });
+                            wx.login({
+                                success: function (res) {
+                                    var code = res.code;
+                                    if (code) {
+                                        wx.getUserInfo({
+                                            success: function (res) {
+                                                Foo.setData({
+                                                    encryptedData: res.encryptedData,
+                                                    iv: res.iv,
+                                                    code: code
+                                                })
+                                            }
+                                        })
+                                    }
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    }, 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+      let that=this;
+      wx.login({
+          success: function (res) {
+              var code = res.code;
+              if (code) {
+                  wx.getUserInfo({
+                      success: function (res) {
+                            that.setData({
+                              encryptedData: res.encryptedData,
+                              iv: res.iv,
+                              code: code
+                          })
+                      }
+                  })
+              }
+          }     
+      })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
